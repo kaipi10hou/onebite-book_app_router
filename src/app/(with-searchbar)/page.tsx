e2979dae9f18acc17ec8b303a545111a4c88a648
kaipi10hou <kaipi10hou@gmail.com>
2024-09-19 09:21:41 +0900

    서버컴포넌트 검색바

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,5 +1,9 @@
+"use client";
+import { useEffect } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
+  console.log("홈컴포넌트 실행");
+  useEffect(() => {});
   return <div className={styles.page}>인덱스페이지</div>;
 }
