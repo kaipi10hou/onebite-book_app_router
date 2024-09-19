ddb1992e8ac67d9cecfe26de870035bf11177962
kaipi10hou <kaipi10hou@gmail.com>
2024-09-19 21:30:12 +0900

    서버컴포넌트 주의사항1

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,9 +1,15 @@
 "use client";
-import { useEffect } from "react";
+import ClientComponent from "./client-component";
 import styles from "./page.module.css";
+import ServerComponent from "./server-component";
 
 export default function Home() {
-  console.log("홈컴포넌트 실행");
-  useEffect(() => {});
-  return <div className={styles.page}>인덱스페이지</div>;
+  return (
+    <div className={styles.page}>
+      인덱스 페이지
+      <ClientComponent>
+        <ServerComponent />
+      </ClientComponent>
+    </div>
+  );
 }
